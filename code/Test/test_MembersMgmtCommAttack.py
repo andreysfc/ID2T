66594f83b5c1cb24618d48e27e214fdcf172cb26
Jens Keim <jens.keim@stud.tu-darmstadt.de>
2018-06-14 11:52:19 +0200

    fix regression test
    change regression hash

--- code/Test/test_MembersMgmtCommAttack.py
+++ code/Test/test_MembersMgmtCommAttack.py
@@ -6,5 +6,5 @@ sha_default = '116b6cb3f1be37e50333a4f1a2535d96b1b053a4c950655391826b43585cff2b'
 
 class UnitTestMembersMgmtCommAttack(Test.ID2TAttackTest):
     def test_regression(self):
-        self.checksum_test([['MembersMgmtCommAttack', 'hidden_mark=True']], sha_default, seed=42,
+        self.checksum_test([['MembersMgmtCommAttack', 'packets.mark=True']], sha_default, seed=42,
                            pcap=Util.TEST_DIR + "reference_telnet.pcap")
